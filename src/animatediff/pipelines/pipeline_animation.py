2a226bde9aa27f82dfbe2a86543eec411799f5f7
Andrew Powers-Holmes <aholmes@omnom.net>
2023-07-23 13:04:43 +1000

    add rich progress bar to pipeline

--- src/animatediff/pipelines/pipeline_animation.py
+++ src/animatediff/pipelines/pipeline_animation.py
@@ -463,3 +463,18 @@ class AnimationPipeline(DiffusionPipeline):
             return video
 
         return AnimationPipelineOutput(videos=video)
+
+    def progress_bar(self, iterable=None, total=None):
+        if not hasattr(self, "_progress_bar_config"):
+            self._progress_bar_config = {}
+        elif not isinstance(self._progress_bar_config, dict):
+            raise ValueError(
+                f"`self._progress_bar_config` should be of type `dict`, but is {type(self._progress_bar_config)}."
+            )
+
+        if iterable is not None:
+            return tqdm(iterable, **self._progress_bar_config)
+        elif total is not None:
+            return tqdm(total=total, **self._progress_bar_config)
+        else:
+            raise ValueError("Either `total` or `iterable` has to be defined.")
