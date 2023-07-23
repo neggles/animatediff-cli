bf5e935de9edb0b3bbfd3d03cf6d57ca969fc4a8
Andrew Powers-Holmes <aholmes@omnom.net>
2023-07-23 20:59:25 +1000

    non-64px-increment resolutions are valid too, unet. shush.

--- src/animatediff/models/unet.py
+++ src/animatediff/models/unet.py
@@ -338,7 +338,7 @@ class UNet3DConditionModel(ModelMixin, ConfigMixin):
         upsample_size = None
 
         if any(s % default_overall_up_factor != 0 for s in sample.shape[-2:]):
-            logger.info("Forward upsample size to force interpolation output size.")
+            logger.debug("Forward upsample size to force interpolation output size.")
             forward_upsample_size = True
 
         # prepare attention_mask
